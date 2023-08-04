f70fc83c08f8f5d952bf3322a8269a7f0a0c2b24
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-08-04 10:18:50 +0200

    Added command runs model

---
+++ euclid/database/models/command_runs.py
@@ -0,0 +1,18 @@
+from peewee import *
+
+from database.models.components.base_models import BaseModel
+from database.models.app import App
+
+
+class CommandRuns(BaseModel):
+    id = AutoField()
+    app = ForeignKeyField(App)
+    hash_id = CharField(unique=True, null=False)
+    command = TextField(null=True)
+    cli_response = TextField(null=True)
+
+    class Meta:
+        db_table = 'command_runs'
+        indexes = (
+            (('app', 'hash_id'), True),
+        )
