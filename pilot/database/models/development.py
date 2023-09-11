367caa1797ac9069127f6230e01cc462bad2dfaf
Goon <duy@wearetopgroup.com>
2023-09-11 10:08:42 +0700

    fix(gitignore): rm pilot-env and cache

--- pilot/database/models/development.py
+++
@@ -1,8 +0,0 @@
-from peewee import *
-
-from database.models.components.progress_step import ProgressStep
-
-
-class Development(ProgressStep):
-    class Meta:
-        db_table = 'development'
--- pilot/db_init.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-from database.database import create_tables, drop_tables
-
-drop_tables()
-create_tables()
