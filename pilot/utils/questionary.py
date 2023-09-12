0619b53d18518f28153f1d505e981b955ee2b46f
Zvonimir Sabljic <zvonimir@pythagora.io>
2023-09-12 21:35:07 +0200

    Necessary flag so 33c38985bf8a355e13c61bb7fc34cd19aa823a2f works

--- pilot/utils/questionary.py
+++ pilot/utils/questionary.py
@@ -18,21 +18,23 @@ def styled_select(*args, **kwargs):
     return questionary.select(*args, **kwargs).unsafe_ask()  # .ask() is included here
 
 
-def styled_text(project, question):
-    project.user_inputs_count += 1
-    user_input = get_user_input_from_hash_id(project, question)
-    if user_input is not None and user_input.user_input is not None and project.skip_steps:
-        # if we do, use it
-        project.checkpoints['last_user_input'] = user_input
-        print(colored(f'Restoring user input id {user_input.id}: ', 'yellow'), end='')
-        print(colored(f'{user_input.user_input}', 'yellow', attrs=['bold']))
-        return user_input.user_input
+def styled_text(project, question, ignore_user_input_count=False):
+    if not ignore_user_input_count:
+        project.user_inputs_count += 1
+        user_input = get_user_input_from_hash_id(project, question)
+        if user_input is not None and user_input.user_input is not None and project.skip_steps:
+            # if we do, use it
+            project.checkpoints['last_user_input'] = user_input
+            print(colored(f'Restoring user input id {user_input.id}: ', 'yellow'), end='')
+            print(colored(f'{user_input.user_input}', 'yellow', attrs=['bold']))
+            return user_input.user_input
 
     config = {
         'style': custom_style,
     }
     response = questionary.text(question, **config).unsafe_ask()  # .ask() is included here
-    user_input = save_user_input(project, question, response)
+    if not ignore_user_input_count:
+        user_input = save_user_input(project, question, response)
 
     print('\n\n', end='')
 
