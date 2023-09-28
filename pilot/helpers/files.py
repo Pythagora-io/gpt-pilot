511ffdd8e1309b36955038841b30330064166503
Nicholas Albion <nalbion@yahoo.com>
2023-09-28 20:08:40 +1000

    Fixed issue with `Project.save_file()`

--- pilot/helpers/files.py
+++ pilot/helpers/files.py
@@ -29,7 +29,7 @@ def get_files_content(directory, ignore=[]):
                 file_content = f.read()
 
             file_name = os.path.basename(path)
-            relative_path = path.replace(directory, '').replace('/' + file_name, '')
+            relative_path = path.replace(directory, '').replace('\\', '/').replace('/' + file_name, '')
             return_array.append({
                 'name': file_name,
                 'path': relative_path,
