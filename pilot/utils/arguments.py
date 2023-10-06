6aa32bdc97961fc99496ab6775718c16d8d34a53
Arnav <62099328+arncv@users.noreply.github.com>
2023-10-06 20:23:08 +0530

    Update arguments.py

--- pilot/utils/arguments.py
+++ pilot/utils/arguments.py
@@ -1,3 +1,4 @@
+import argparse
 import hashlib
 import os
 import re
@@ -9,67 +10,59 @@ from utils.style import green_bold
 
 
 def get_arguments():
-    # The first element in sys.argv is the name of the script itself.
-    # Any additional elements are the arguments passed from the command line.
-    args = sys.argv[1:]
+    
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser()
 
-    # Create an empty dictionary to store the key-value pairs.
-    arguments = {}
+    # Add command-line arguments with their types and default values
+    parser.add_argument('--user_id', type=str, default=username_to_uuid(getuser()))
+    parser.add_argument('--workspace', type=str, default=None)
+    parser.add_argument('--app_id', type=str, default=str(uuid.uuid4()))
+    parser.add_argument('--email', type=str, default=get_email())
+    parser.add_argument('--password', type=str, default='password')
+    parser.add_argument('--step', type=str, default=None)
 
-    # Loop through the arguments and parse them as key-value pairs.
-    for arg in args:
-        if '=' in arg:
-            key, value = arg.split('=', 1)
-            arguments[key] = value
-        else:
-            arguments[arg] = True
-
-    if 'user_id' not in arguments:
-        arguments['user_id'] = username_to_uuid(getuser())
-
+    # Parse the command-line arguments
+    arguments = parser.parse_args()
+    
+    # Initialize app as None
     app = None
-    if 'workspace' in arguments:
-        app = get_app_by_user_workspace(arguments['user_id'], arguments['workspace'])
+    
+    # If workspace is provided, get the corresponding app
+    if arguments.workspace:
+        app = get_app_by_user_workspace(arguments.user_id, arguments.workspace)
         if app is not None:
-            arguments['app_id'] = app.id
+            arguments.app_id = app.id
     else:
-        arguments['workspace'] = None
+        arguments.workspace = None
 
-    if 'app_id' in arguments:
+    # If app_id is provided, get the app details and print them
+    if arguments.app_id:
         try:
             if app is None:
-                app = get_app(arguments['app_id'])
+                app = get_app(arguments.app_id)
 
-            arguments['app_type'] = app.app_type
-            arguments['name'] = app.name
-            arguments['step'] = app.status
+            arguments.app_type = app.app_type
+            arguments.name = app.name
+            arguments.step = app.status
             # Add any other fields from the App model you wish to include
 
             print(green_bold('\n------------------ LOADING PROJECT ----------------------'))
-            print(green_bold(f'{app.name} (app_id={arguments["app_id"]})'))
+            print(green_bold(f'{app.name} (app_id={arguments.app_id})'))
             print(green_bold('--------------------------------------------------------------\n'))
         except ValueError as e:
             print(e)
             # Handle the error as needed, possibly exiting the script
     else:
-        arguments['app_id'] = str(uuid.uuid4())
+        # If app_id is not provided, print details for starting a new project
         print(green_bold('\n------------------ STARTING NEW PROJECT ----------------------'))
         print("If you wish to continue with this project in future run:")
-        print(green_bold(f'python {sys.argv[0]} app_id={arguments["app_id"]}'))
+        print(green_bold(f'python {sys.argv[0]} app_id={arguments.app_id}'))
         print(green_bold('--------------------------------------------------------------\n'))
 
-    if 'email' not in arguments:
-        arguments['email'] = get_email()
-
-    if 'password' not in arguments:
-        arguments['password'] = 'password'
-
-    if 'step' not in arguments:
-        arguments['step'] = None
-
+    # Return the parsed arguments
     return arguments
-
-
+    
 def get_email():
     # Attempt to get email from .gitconfig
     gitconfig_path = os.path.expanduser('~/.gitconfig')
