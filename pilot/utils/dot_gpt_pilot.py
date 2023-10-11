1f0ed3e36b851ecdd43845d7bc0588605af97781
Nicholas Albion <nalbion@yahoo.com>
2023-10-12 10:35:23 +1100

    disable `.gpt-pilot/` by default

--- pilot/utils/dot_gpt_pilot.py
+++ pilot/utils/dot_gpt_pilot.py
@@ -2,6 +2,11 @@ import json
 import os
 import yaml
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USE_GPTPILOT_FOLDER = os.getenv('USE_GPTPILOT_FOLDER') == 'true'
 
 
 # TODO: Parse files from the `.gpt-pilot` directory to resume a project - `user_stories` may have changed - include checksums for sections which may need to be reprocessed.
@@ -11,11 +16,15 @@ class DotGptPilot:
     Manages the `.gpt-pilot` directory.
     """
     def __init__(self, log_chat_completions: bool = True):
+        if not USE_GPTPILOT_FOLDER:
+            return
         self.log_chat_completions = log_chat_completions
         self.dot_gpt_pilot_path = self.with_root_path('~', create=False)
         self.chat_log_path = self.chat_log_folder(None)
 
     def with_root_path(self, root_path: str, create=True):
+        if not USE_GPTPILOT_FOLDER:
+            return
         dot_gpt_pilot_path = os.path.join(root_path, '.gpt-pilot')
         self.dot_gpt_pilot_path = dot_gpt_pilot_path
 
@@ -26,6 +35,8 @@ class DotGptPilot:
         return dot_gpt_pilot_path
 
     def chat_log_folder(self, task):
+        if not USE_GPTPILOT_FOLDER:
+            return
         chat_log_path = os.path.join(self.dot_gpt_pilot_path, 'chat_log')
         if task is not None:
             chat_log_path = os.path.join(chat_log_path, 'task_' + task)
@@ -35,6 +46,8 @@ class DotGptPilot:
         return chat_log_path
 
     def log_chat_completion(self, endpoint: str, model: str, req_type: str, messages: list[dict], response: str):
+        if not USE_GPTPILOT_FOLDER:
+            return
         if self.log_chat_completions:
             time = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
             with open(os.path.join(self.chat_log_path, f'{time}-{req_type}.yaml'), 'w') as file:
@@ -48,6 +61,8 @@ class DotGptPilot:
                 yaml.safe_dump(data, file, width=120, indent=2, default_flow_style=False, sort_keys=False)
 
     def log_chat_completion_json(self, endpoint: str, model: str, req_type: str, functions: dict, json_response: str):
+        if not USE_GPTPILOT_FOLDER:
+            return
         if self.log_chat_completions:
             time = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
 
@@ -62,6 +77,8 @@ class DotGptPilot:
                 json.dump(data, file, indent=2)
 
     def write_project(self, project):
+        if not USE_GPTPILOT_FOLDER:
+            return
         data = {
             'name': project.args['name'],
             'description': project.project_description,
