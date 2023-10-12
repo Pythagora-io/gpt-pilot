ac30da62e0c53b17f228e7f00671b9ec776f7a3e
Nicholas Albion <nalbion@yahoo.com>
2023-10-12 16:55:27 +1100

    updated tests

--- pilot/helpers/test_cli.py
+++ pilot/helpers/test_cli.py
@@ -2,5 +2,5 @@ from pilot.helpers.cli import terminate_process
 
 
 def test_terminate_process_not_running():
-    terminate_process('999999999', 'not running')
+    terminate_process(999999999, 'not running')
     assert True
