2b4c2b7aa364d7875659f4e4aa2a1d572eea6443
Nicholas Albion <nalbion@yahoo.com>
2023-10-12 17:06:56 +1100

    fixed test

--- pilot/utils/test_files.py
+++ pilot/utils/test_files.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from .files import setup_workspace
 
 
@@ -15,11 +16,20 @@ def mocked_abspath(file):
     return "/root_path/pilot/helpers"
 
 
-def test_setup_workspace_without_existing_workspace(monkeypatch):
-    args = {'workspace': None, 'name': 'project_name'}
+def test_setup_workspace_with_root_arg(monkeypatch):
+    args = {'root': '/my/root', 'name': 'project_name'}
 
     monkeypatch.setattr('os.path.abspath', mocked_abspath)
     monkeypatch.setattr('os.makedirs', mocked_create_directory)
+
+    result = setup_workspace(args)
+    assert result.replace('\\', '/') == "/my/root/project_name"
+
+
+@patch('utils.files.os.path.abspath', return_value='/root_path/pilot/helpers')
+@patch('utils.files.os.makedirs', side_effect=mocked_create_directory)
+def test_setup_workspace_without_existing_workspace(mock_makedirs, mock_abs_path):
+    args = {'workspace': None, 'name': 'project_name'}
 
     result = setup_workspace(args)
     assert result.replace('\\', '/') == "/root_path/workspace/project_name"
