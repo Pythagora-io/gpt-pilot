326d7df63efdb276bf212de8676d2170db508605
Nicholas Albion <nalbion@yahoo.com>
2023-10-13 16:13:30 +1100

    fix for #195 - missing positional arg `max_tokens`

--- pilot/helpers/exceptions/TokenLimitError.py
+++ pilot/helpers/exceptions/TokenLimitError.py
@@ -1,5 +1,8 @@
+from const.llm import MAX_GPT_MODEL_TOKENS
+
+
 class TokenLimitError(Exception):
-    def __init__(self, tokens_in_messages, max_tokens):
+    def __init__(self, tokens_in_messages, max_tokens=MAX_GPT_MODEL_TOKENS):
         self.tokens_in_messages = tokens_in_messages
         self.max_tokens = max_tokens
         super().__init__(f"Token limit error happened with {tokens_in_messages}/{max_tokens} tokens in messages!")
