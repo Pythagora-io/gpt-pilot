4ee48158a299772de6174cdf5887d7c943e7ed23
Arno.Edwards <138990495+Umpire2018@users.noreply.github.com>
2023-10-19 17:37:44 +0800

    fix test `ValueError: binary mode doesn't take an encoding argument`

--- pilot/utils/custom_open.py
+++ pilot/utils/custom_open.py
@@ -5,8 +5,8 @@ built_in_open = builtins.open
 
 
 def open(file, *args, **kwargs):
-    # If encoding is not specified, set it to 'utf-8'
-    if 'encoding' not in kwargs:
+    # If encoding is not specified and it's not binary mode, set it to 'utf-8'
+    if 'encoding' not in kwargs and 'b' not in args:
         kwargs['encoding'] = 'utf-8'
 
     # Call the original 'open' function with potentially modified arguments
