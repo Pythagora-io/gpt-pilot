09f1c8451efbaf930fa9165658d405d95ff5aaaa
LeonOstrez <leon.ostrez@gmail.com>
2023-11-01 14:06:22 +0000

    add exit message

--- pilot/const/ipc.py
+++ pilot/const/ipc.py
@@ -8,10 +8,12 @@ MESSAGE_TYPE = {
     'run_command': 'run_command',          # Command to run server needed for extension only
     'project_folder_name': 'project_folder_name',  # Project folder name for extension only
     'button': 'button',                    # Button text for extension only
+    'exit': 'exit',                        # Exit message to let extension know we are done
 }
 
 LOCAL_IGNORE_MESSAGE_TYPES = [
     'info',
     'project_folder_name',
     'button',
+    'exit',
 ]
