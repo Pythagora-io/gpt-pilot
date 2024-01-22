16c6c43f73c93ec05e03193bb3093d45921df725
Sebastian <sebastian.andersson3@gmail.com>
2024-01-22 11:56:44 +0100

    clarification on check-up statement for continued flow

--- pilot/const/messages.py
+++ pilot/const/messages.py
@@ -1,4 +1,4 @@
-CHECK_AND_CONTINUE = 'If something is wrong, tell me or type "continue" to continue.'
-WHEN_USER_DONE = 'Once you\'re done, type "continue"'
-AFFIRMATIVE_ANSWERS = ['', 'y', 'yes', 'ok', 'okay', 'sure']
-NEGATIVE_ANSWERS = ['n', 'no', 'skip']
+CHECK_AND_CONTINUE = 'Please inform me of any issues, or type "continue" to proceed.'
+WHEN_USER_DONE = 'Once you have completed, enter "continue"'
+AFFIRMATIVE_ANSWERS = ['', 'y', 'yes', 'ok', 'okay', 'sure', 'absolutely', 'indeed', 'correct', 'affirmative']
+NEGATIVE_ANSWERS = ['n', 'no', 'skip', 'negative', 'not now', 'cancel', 'decline', 'stop']
