7f700a335739c9d482e5e9228133bdfaa24e62ce
Zvonimir Sabljic <zvonimir@pythagora.io>
2024-01-27 20:23:34 -0800

    Revised some copies

--- pilot/const/messages.py
+++ pilot/const/messages.py
@@ -1,4 +1,4 @@
-CHECK_AND_CONTINUE = 'Please inform me of any issues, or type "continue" to proceed.'
+CHECK_AND_CONTINUE = 'Is everything working? Let me know if something needs to be changed for this task or type "continue" to proceed.'
 WHEN_USER_DONE = 'Once you have completed, enter "continue"'
-AFFIRMATIVE_ANSWERS = ['', 'y', 'yes', 'ok', 'okay', 'sure', 'absolutely', 'indeed', 'correct', 'affirmative']
-NEGATIVE_ANSWERS = ['n', 'no', 'skip', 'negative', 'not now', 'cancel', 'decline', 'stop']
+AFFIRMATIVE_ANSWERS = ['', 'y', 'yes', 'ok', 'okay', 'sure', 'absolutely', 'indeed', 'correct', 'affirmative', 'Use GPT Pilot\'s code']
+NEGATIVE_ANSWERS = ['n', 'no', 'skip', 'negative', 'not now', 'cancel', 'decline', 'stop', 'Keep my changes']
