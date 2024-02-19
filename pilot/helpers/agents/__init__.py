b2128adc68c0418154cc4a8f9314acc9fb65b781
Aaron Job <awjob42@gmail.com>
2024-02-19 04:15:45 +0000

    fixed test_product_owner_model_override

--- pilot/helpers/agents/__init__.py
+++ pilot/helpers/agents/__init__.py
@@ -3,3 +3,4 @@ from .Developer import Developer, ENVIRONMENT_SETUP_STEP
 from .TechLead import TechLead
 from .TechnicalWriter import TechnicalWriter
 from .CodeMonkey import CodeMonkey
+from .ProductOwner import ProductOwner
--- pilot/helpers/agents/test_ProductOwner.py
+++ pilot/helpers/agents/test_ProductOwner.py
@@ -2,26 +2,26 @@ import builtins
 import os
 from unittest.mock import patch
 from dotenv import load_dotenv
-from pilot.helpers.agents import ProductOwner
+from helpers.agents import ProductOwner
 
-from pilot.helpers.test_Project import create_project
+from helpers.test_Project import create_project
 load_dotenv()
 
 from main import get_custom_print
 
 
-class TestTechLead:
+class TestProductOwner:
     def setup_method(self):
         builtins.print, ipc_client_instance = get_custom_print({})
 
-        name = 'TestArchitect'
+        name = 'TestProductOwner'
         self.project = create_project()
-        self.project.app_id = 'test-architect'
+        self.project.app_id = 'test-product-owner'
         self.project.name = name
 
 
         self.project.set_root_path(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                              '../../../workspace/TestArchitect')))
+                                                              '../../../workspace/TestProductOwner')))
 
     def test_product_owner_model_override(self, monkeypatch):
         # Given any project
