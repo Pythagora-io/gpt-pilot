3da51e9d2e35c9d1a6ca98adeb6ecff161530fa1
LeonOstrez <leon.ostrez@gmail.com>
2024-02-27 22:44:46 -0800

    change detection of loops from steps to iterations

--- pilot/const/telemetry.py
+++ pilot/const/telemetry.py
@@ -1,3 +1,3 @@
 LARGE_REQUEST_THRESHOLD = 50000  # tokens
 SLOW_REQUEST_THRESHOLD = 300  # seconds
-LOOP_THRESHOLD = 20  # number of steps in task to be considered a loop
+LOOP_THRESHOLD = 3  # number of iterations in task to be considered a loop
