01c40eb4fcaa26e2e7377acb8bdbfad21110f5a1
Senko Rasic <senko.rasic@dobarkod.hr>
2024-03-05 11:34:15 +0100

    update tests to not fail on v0.1.x

--- pilot/test/utils/test_settings.py
+++ pilot/test/utils/test_settings.py
@@ -143,7 +143,7 @@ def test_get_git_commit():
 
 
 def test_get_package_version():
-    assert get_package_version().startswith("0.0.")
+    assert get_package_version().startswith("0.")
 
 
 def test_get_version():
