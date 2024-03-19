deea438008727c8e39d35d45117cb34a0f1a6cad
Senko Rasic <senko@senko.net>
2024-03-18 19:04:54 -0700

    update tests as well for the dotenv override change

--- pilot/helpers/test_AgentConvo.py
+++ pilot/helpers/test_AgentConvo.py
@@ -8,7 +8,7 @@ from helpers.AgentConvo import AgentConvo
 from utils.custom_print import get_custom_print
 from .test_Project import create_project
 
-load_dotenv()
+load_dotenv(override=True)
 
 builtins.print, ipc_client_instance = get_custom_print({})
 
