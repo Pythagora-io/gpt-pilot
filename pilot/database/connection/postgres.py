0f428bd17b29eac5ddf0fe037a8e336cfcacbe11
TonyB <zbtsebuaa@outlook.com>
2024-05-11 13:43:08 +0800

    reformat codes

--- pilot/database/connection/postgres.py
+++ pilot/database/connection/postgres.py
@@ -1,11 +1,14 @@
 from peewee import PostgresqlDatabase
 from database.config import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DATABASE_TYPE
+
 if DATABASE_TYPE == "postgres":
     import psycopg2
     from psycopg2.extensions import quote_ident
 
+
 def get_postgres_database():
     return PostgresqlDatabase(DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+
 
 def create_postgres_database():
     conn = psycopg2.connect(
