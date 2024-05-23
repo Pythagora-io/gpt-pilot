0429614a4b721a751da8da44ca6323120c9828bb
Goran Peretin <goran.peretin@gmail.com>
2024-05-23 10:22:54 +0000

    Inline ifs

--- core/state/state_manager.py
+++ core/state/state_manager.py
@@ -167,15 +167,14 @@ class StateManager:
             f"step {state.step_index} (state id={state.id})"
         )
 
-        if self.current_state.current_epic:
-            if self.ui:
-                await self.ui.send_task_progress(
-                    self.current_state.tasks.index(self.current_state.current_task) + 1,
-                    len(self.current_state.tasks),
-                    self.current_state.current_task["description"],
-                    self.current_state.current_epic.get("source", "app"),
-                    "in-progress",
-                )
+        if self.current_state.current_epic and self.ui:
+            await self.ui.send_task_progress(
+                self.current_state.tasks.index(self.current_state.current_task) + 1,
+                len(self.current_state.tasks),
+                self.current_state.current_task["description"],
+                self.current_state.current_epic.get("source", "app"),
+                "in-progress",
+            )
 
         return self.current_state
 
