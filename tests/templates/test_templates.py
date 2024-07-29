b0e6ea72906e977522cae8b79625ae90dc1924fe
LeonOstrez <leon.ostrez@gmail.com>
2024-07-29 10:40:50 +0200

    skip tests instead of commenting them out

--- tests/templates/test_templates.py
+++ tests/templates/test_templates.py
@@ -5,53 +5,56 @@ import pytest
 from core.state.state_manager import StateManager
 from core.templates.registry import PROJECT_TEMPLATES
 
-# @pytest.mark.asyncio
-# @patch("core.state.state_manager.get_config")
-# async def test_render_react_express_sql(mock_get_config, testmanager):
-#     mock_get_config.return_value.fs.type = "memory"
-#     sm = StateManager(testmanager)
-#     pm = MagicMock(run_command=AsyncMock())
-#
-#     await sm.create_project("TestProjectName")
-#     await sm.commit()
-#
-#     TemplateClass = PROJECT_TEMPLATES["react_express"]
-#     options = TemplateClass.options_class(db_type="sql", auth=True)
-#     template = TemplateClass(options, sm, pm)
-#
-#     assert template.options_dict == {"db_type": "sql", "auth": True}
-#
-#     await template.apply()
-#
-#     files = sm.file_system.list()
-#     for f in ["server.js", "index.html", "prisma/schema.prisma", "api/routes/authRoutes.js", "ui/pages/Register.jsx"]:
-#         assert f in files
-#     assert "api/models/user.js" not in files
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+@patch("core.state.state_manager.get_config")
+async def test_render_react_express_sql(mock_get_config, testmanager):
+    mock_get_config.return_value.fs.type = "memory"
+    sm = StateManager(testmanager)
+    pm = MagicMock(run_command=AsyncMock())
+
+    await sm.create_project("TestProjectName")
+    await sm.commit()
+
+    TemplateClass = PROJECT_TEMPLATES["react_express"]
+    options = TemplateClass.options_class(db_type="sql", auth=True)
+    template = TemplateClass(options, sm, pm)
+
+    assert template.options_dict == {"db_type": "sql", "auth": True}
+
+    await template.apply()
+
+    files = sm.file_system.list()
+    for f in ["server.js", "index.html", "prisma/schema.prisma", "api/routes/authRoutes.js", "ui/pages/Register.jsx"]:
+        assert f in files
+    assert "api/models/user.js" not in files
 
 
-# @pytest.mark.asyncio
-# @patch("core.state.state_manager.get_config")
-# async def test_render_react_express_nosql(mock_get_config, testmanager):
-#     mock_get_config.return_value.fs.type = "memory"
-#     sm = StateManager(testmanager)
-#     pm = MagicMock(run_command=AsyncMock())
-#
-#     await sm.create_project("TestProjectName")
-#     await sm.commit()
-#
-#     TemplateClass = PROJECT_TEMPLATES["react_express"]
-#     options = TemplateClass.options_class(db_type="nosql", auth=True)
-#     template = TemplateClass(options, sm, pm)
-#
-#     assert template.options_dict == {"db_type": "nosql", "auth": True}
-#
-#     await template.apply()
-#
-#     files = sm.file_system.list()
-#     print(files)
-#     for f in ["server.js", "index.html", "api/models/user.js", "api/routes/authRoutes.js", "ui/pages/Register.jsx"]:
-#         assert f in files
-#     assert "prisma/schema.prisma" not in files
+@pytest.mark.skip
+@pytest.mark.asyncio
+@patch("core.state.state_manager.get_config")
+async def test_render_react_express_nosql(mock_get_config, testmanager):
+    mock_get_config.return_value.fs.type = "memory"
+    sm = StateManager(testmanager)
+    pm = MagicMock(run_command=AsyncMock())
+
+    await sm.create_project("TestProjectName")
+    await sm.commit()
+
+    TemplateClass = PROJECT_TEMPLATES["react_express"]
+    options = TemplateClass.options_class(db_type="nosql", auth=True)
+    template = TemplateClass(options, sm, pm)
+
+    assert template.options_dict == {"db_type": "nosql", "auth": True}
+
+    await template.apply()
+
+    files = sm.file_system.list()
+    print(files)
+    for f in ["server.js", "index.html", "api/models/user.js", "api/routes/authRoutes.js", "ui/pages/Register.jsx"]:
+        assert f in files
+    assert "prisma/schema.prisma" not in files
 
 
 @pytest.mark.skip
