94a5c77a80f6423700c1f1a5c6f0740042adce2b
Zvonimir Sabljic <zvonimir@pythagora.io>
2024-08-01 21:29:36 -0700

    Removed Senkos template for now

--- core/templates/registry.py
+++ core/templates/registry.py
@@ -4,7 +4,8 @@ from core.log import get_logger
 
 from .javascript_react import JavascriptReactProjectTemplate
 from .node_express_mongoose import NodeExpressMongooseProjectTemplate
-from .react_express import ReactExpressProjectTemplate
+
+# from .react_express import ReactExpressProjectTemplate
 
 log = get_logger(__name__)
 
@@ -14,11 +15,11 @@ class ProjectTemplateEnum(str, Enum):
 
     JAVASCRIPT_REACT = JavascriptReactProjectTemplate.name
     NODE_EXPRESS_MONGOOSE = NodeExpressMongooseProjectTemplate.name
-    REACT_EXPRESS = ReactExpressProjectTemplate.name
+    # REACT_EXPRESS = ReactExpressProjectTemplate.name
 
 
 PROJECT_TEMPLATES = {
     JavascriptReactProjectTemplate.name: JavascriptReactProjectTemplate,
     NodeExpressMongooseProjectTemplate.name: NodeExpressMongooseProjectTemplate,
-    ReactExpressProjectTemplate.name: ReactExpressProjectTemplate,
+    # ReactExpressProjectTemplate.name: ReactExpressProjectTemplate,
 }
