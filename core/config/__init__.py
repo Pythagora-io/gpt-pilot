3405cb8f7b25f193b9b5b1249b3283b368b38590
LeonOstrez <leon.ostrez@gmail.com>
2024-09-20 13:17:08 +0200

    increase API timeout

--- core/config/__init__.py
+++ core/config/__init__.py
@@ -156,7 +156,7 @@ class LLMConfig(_StrictModel):
         ge=0.0,
     )
     read_timeout: float = Field(
-        default=10.0,
+        default=20.0,
         description="Timeout (in seconds) for receiving a new chunk of data from the response stream",
         ge=0.0,
     )
