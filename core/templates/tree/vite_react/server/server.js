b57d9131dbaa22923e32796202c4915cf423b409
Zvonimir Sabljic <zvonimir@pythagora.io>
2025-01-14 14:54:59 +0100

    Fix

--- core/templates/tree/vite_react/server/server.js
+++ core/templates/tree/vite_react/server/server.js
@@ -27,8 +27,10 @@ app.use(cors({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+{% if options.auth %}
 // Authentication routes
 app.use(authRoutes);
+{% endif %}
 
 // Database connection
 connectDB();
